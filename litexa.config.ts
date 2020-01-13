685f56731d77be576719b5e0620a158bf40e3f8f
Dylan House <drhouse@amazon.com>
2020-01-13 11:22:18 -0800

    .gitignore update

--- litexa.config.ts
+++ litexa.config.ts
@@ -8,11 +8,11 @@
 const deploymentConfiguration: Configuration = {
     name: 'cv-quickdraw',
     deployments: {
-        development: {
+        drhouse: {
             module: '@litexa/deploy-aws',
-            s3Configuration: { bucketName: '' },
-            askProfile: '',
-            awsProfile: ''
+            s3Configuration: { bucketName: 'cv-quickdraw' },
+            askProfile: 'diamond-games-devs',
+            awsProfile: 'drhouse'
         }
     },
     plugins: {}
